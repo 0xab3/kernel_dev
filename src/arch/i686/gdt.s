39e1b727311f6f0acda4dd98cf4a9901534ede77
shahzad <shahzadakhtar182005@gmail.com>
2024-11-16 15:53:37 +0530

    feat(memory): added support for gdt i hope

--- src/arch/i686/gdt.s
+++ src/arch/i686/gdt.s
@@ -2,6 +2,17 @@
 .type gdt_init, @function
 
 gdt_init:
-  mov  0x12( %ebp ), %eax 
-  mov %al, 0xb8000
+  cli
+  mov 4(%esp), %eax
+  lgdt (%eax)
+  ljmp $0x8,$reload_CS
+reload_CS:
+  mov %ax, 0x10 
+  mov %ds, %ax
+  mov %es, %ax
+  mov %fs, %ax
+  mov %gs, %ax
+  mov %ss, %ax
+  nop
+  sti 
   ret
--- src/include/gdt.h
+++ src/include/gdt.h
@@ -2,6 +2,22 @@
 #define GDT_H
 #include "common.h"
 #include <stdint.h>
-struct global_descriptor_table {};
-extern void gdt_init(int32_t arg);
+typedef int64_t gdt_format_t;
+
+struct __attribute__((packed)) gdt_description {
+  int16_t size;
+  gdt_format_t* offset;
+};
+
+// note(shahzad): no need to give a shit abt padding struct it's only for convenience
+struct gdt_entry {
+  uint32_t limit: 20;
+  uint32_t base;
+  int8_t access_byte;
+  uint8_t flags: 4;
+};
+
+extern void gdt_init(struct gdt_description* arg);
+gdt_format_t gdt_to_anal_format(struct gdt_entry entry);
+
 #endif
