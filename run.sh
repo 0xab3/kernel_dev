e43a5fa67ee90b1162dd7984d107b87b129fbf45
shahzad <shahzadakhtar182005@gmail.com>
2024-11-17 19:01:31 +0530

    chore: add debug and build in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-python3 ./src/utils/bin2c.py ./src/image.raw -O ./src/image.raw.h
-
+zig build
 mkdir -p isodir/boot/grub
 cp ./zig-out/bin/kernel.elf ./isodir/boot/myos.bin
 cp ./src/grub.cfg isodir/boot/grub/grub.cfg
@@ -9,4 +8,5 @@ objcopy --only-keep-debug ./zig-out/bin/kernel.elf ./zig-out/kernel.sym
 objcopy --strip-debug ./zig-out/bin/kernel.elf
 
 grub-mkrescue -o iosbuild/myos.iso isodir
-qemu-system-i386 -cdrom iosbuild/myos.iso
+# qemu-system-i386 -s -S -cdrom iosbuild/myos.iso
+qemu-system-i386 -serial stdio -cdrom iosbuild/myos.iso
