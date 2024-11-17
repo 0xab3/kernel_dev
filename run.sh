63d2fbc002ac1e83532af179abcefbc90d6a0419
shahzad <shahzadakhtar182005@gmail.com>
2024-11-17 22:40:10 +0530

    rename(run.sh): isobuild -> iosbuild

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ cp ./src/grub.cfg isodir/boot/grub/grub.cfg
 objcopy --only-keep-debug ./zig-out/bin/kernel.elf ./zig-out/kernel.sym
 objcopy --strip-debug ./zig-out/bin/kernel.elf
 
-grub-mkrescue -o iosbuild/myos.iso isodir
-# qemu-system-i386 -s -S -cdrom iosbuild/myos.iso
-qemu-system-i386 -serial stdio -cdrom iosbuild/myos.iso
+mkdir isobuild
+grub-mkrescue ./isodir -o isobuild/myos.iso
+# qemu-system-i386 -s -S -cdrom isobuil/myos.iso
+qemu-system-i386 -serial stdio -cdrom isobuild/myos.iso
