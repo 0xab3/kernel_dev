751ce5121b75322bc2f9ef885a3070ab68caad31
shahzad <shahzadakhtar182005@gmail.com>
2024-11-18 19:01:40 +0530

    enhance(debug): don't strip debug info

--- run.sh
+++ run.sh
@@ -5,9 +5,8 @@ cp ./zig-out/bin/kernel.elf ./isodir/boot/myos.bin
 cp ./src/grub.cfg isodir/boot/grub/grub.cfg
 
 objcopy --only-keep-debug ./zig-out/bin/kernel.elf ./zig-out/kernel.sym
-objcopy --strip-debug ./zig-out/bin/kernel.elf
 
 mkdir isobuild
 grub-mkrescue ./isodir -o isobuild/myos.iso
-# qemu-system-i386 -s -S -cdrom isobuil/myos.iso
+# qemu-system-i386 -s -S -cdrom isobuild/myos.iso
 qemu-system-i386 -serial stdio -cdrom isobuild/myos.iso
