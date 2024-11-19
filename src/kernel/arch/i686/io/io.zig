bf625f8e0fd8eee3121b80d39d4f68e76c76f515
shahzad <shahzadakhtar182005@gmail.com>
2024-11-20 02:31:32 +0530

    fix(io): fix inb function
    inb was not using correct instruction to read io ports, fixed that

--- src/kernel/arch/i686/io/io.zig
+++ src/kernel/arch/i686/io/io.zig
@@ -6,10 +6,17 @@ pub fn outb(port: u16, byte: u8) void {
           [byte] "{al}" (byte),
     );
 }
+// (todo):shahzad make it generic over type inb, inw,indw
 pub fn inb(port: u16) u8 {
     return asm volatile (
-        \\outb %[ret], %[port]
+        \\inb %[port], %[ret]
         : [ret] "=r" (-> u8),
-        : [port] "{dx}" (port),
+        : [port] "N{dx}" (port),
     );
+}
+
+//NOTE: wait a very small amount of time (1 to 4 microseconds, generally). Useful for implementing a small delay for PIC remapping on old hardware or generally as a simple but imprecise wait.
+//NOTE!: you can do an IO operation on any unused port: the Linux kernel by default uses port 0x80, which is often used during POST to log information on the motherboard's hex display but almost always unused after boot.
+pub fn wait() void {
+    outb(0x80, 0);
 }
