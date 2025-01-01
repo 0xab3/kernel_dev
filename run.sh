c40971cda4b4c945cf47031a46892d96fec66c1f
-0xab3 <shahzadakhtar182005@gmail.com>
2025-01-01 11:47:06 +0530

    chore: fail run script on error

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+set -xe
 zig build
-mkdir -p isodir/boot/grub
+mkdir -p isodir/boot/grub || true
 cp ./zig-out/bin/kernel.elf ./isodir/boot/myos.bin
 cp ./src/kernel/grub.cfg isodir/boot/grub/grub.cfg
 
 objcopy --only-keep-debug ./zig-out/bin/kernel.elf ./zig-out/kernel.sym
 
-mkdir isobuild
+mkdir isobuild || true
 grub-mkrescue ./isodir -o isobuild/myos.iso
-# qemu-system-i386 -s -S -cdrom isobuild/myos.iso
 qemu-system-i386 -serial stdio -cdrom isobuild/myos.iso
